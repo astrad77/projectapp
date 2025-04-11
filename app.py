f03978af3d22dc53f88fc802e95ba218db3e6a08
astrad77 <astrad77@gmail.com>
2025-04-11 14:37:29 -0600

    added dependency for smoother

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import statsmodels 
 
 # Page config
 st.set_page_config(page_title="Electricity and Renewables", layout="wide")
